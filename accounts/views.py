55373cddeefef940f08ec13aedfefd434d8d1251
Hesam <hesam.ghadami2020@gmail.com>
2023-12-12 22:50:59 +0330

    mckxjx

--- accounts/views.py
+++ accounts/views.py
@@ -30,3 +30,15 @@ class SignUpView(CreateView):
      template_name = 'registrations/signup.html'
      form_class = CustomUserCreation
      success_url = '/accounts/login/'
+     
+     def form_valid(self, form):
+        form.save()
+        email = self.request.POST.get('email')
+        password = self.request.POST.get('password1')   
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(self.request,user)
+            return redirect('/')
+        
+     def form_invalid(self, form):
+        return super().form_invalid(form)
